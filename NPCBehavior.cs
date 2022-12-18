95671f9ae9905614ddd3ce8430442797f79d7ac7
SamSamovich <68682003+SamSamovich@users.noreply.github.com>
2022-12-18 18:43:54 +0600

    Add files via upload

---
+++ NPCBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCBehavior : MonoBehaviour
+{
+    int health;
+    int level;
+    int speed = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        health += level; //К здоровью прибавляется уровень NPC 
+        print(health); //Выводит значение HP в консоль
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
